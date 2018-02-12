af02944bfcac524c2c6b93d83e3a72f5cfa6db56
Basavaraj K N <Basavaraj.K@Stackroute.in>
2018-02-12 23:51:11 +0530

    Integration of GraphQL with Redux

--- src/index.js
+++ src/index.js
@@ -5,17 +5,35 @@ import App from './App';
 import ActionReducers from './ActionReducers';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: '/' }),
+  cache: new InMemoryCache()
+});
+
 // Enhance the redux store with middleware (add a chain, between dispatching of action, and these actions reaching the reducers)
-let store = createStore(ActionReducers, applyMiddleware(thunkMiddleware));
+let store = createStore(ActionReducers,
+		compose(
+				applyMiddleware(thunkMiddleware),
+				// If you are using the devToolsExtension, you can add it here also
+      	(typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined') ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+			)
+	);
 
 function render() {
 	return (
 		<Provider store={store}>
-			<App />
+			<ApolloProvider client={client}>
+				<App />
+			</ApolloProvider>
 		</Provider>
 	)
 }
